ec581acdfa1bac902f654595312f67454a92bdb7
Antun Kakuk <kuka.251@hotmail.com>
2020-07-03 10:26:45 +0200

    TLR 2.0

--- houghCircleTransform.py
+++ houghCircleTransform.py
@@ -5,19 +5,35 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-
-start = time.perf_counter()
-
-
  
-img = cv2.imread("25914.png")
+img = cv2.imread("15096.png")
+# img = cv2.imread("15026.png")
+# img = cv2.imread("37596.png")
+# img = cv2.imread("22008.png")
+# img = cv2.imread("14888.png")
+# img = cv2.imread("11342.png")
+# img = cv2.imread("14678.png")
+# img = cv2.imread("14748.png")
+# img = cv2.imread("14956.png")
+# img = cv2.imread("18008.png")
+# img = cv2.imread("25568.png")
+# img = cv2.imread("25914.png")
+# img = cv2.imread("40578.png")
+# img = cv2.imread("42454.png")
+# img = cv2.imread("52890.png")
+# img = cv2.imread("72104.png")
+# img = cv2.imread("77280.png")
+# img = cv2.imread("432522.png")
+# img = cv2.imread("436246.png")
+# img = cv2.imread("511520.png")
+
 #img = cv2.imread("houghCircle.png")
 img = img[0:int(img.shape[0]/2), 0:int(img.shape[1]), 0:3].copy()
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_blur = cv2.blur(img_gray, (5, 5))
 img_non_filter = cv2.Canny(img_blur, 100, 200)
 
-
+minimumDistanceBetweenCircles = 50;
  
 
 
@@ -32,11 +48,12 @@ cv2.imshow("No filter", img_non_filter)
 cv2.imshow("And", img_and)
 cv2.waitKey()
 cv2.destroyAllWindows()
+start = time.perf_counter()
 
-img = img_filter
 imgOrg = img
+img = img_and
 maxRad = 12
-minRad = 2
+minRad = 3
 
 acc = np.zeros((img.shape[1], img.shape[0], maxRad - minRad))
 #print(img.shape[1])
@@ -46,8 +63,8 @@ for w in range(0, img.shape[1] - 1):
         if img[h, w] == 255:
             for r in range(0, maxRad - minRad):
                 for th in range(0, 360):
-                    a = int(round(w - (r + minRad) * np.cos((th * np.pi) / 180)))
-                    b = int(round(h - (r + minRad) * np.sin((th * np.pi) / 180)))
+                    a = int(round(w - (r + minRad) * np.cos((th * np.pi) / 180))) 
+                    b = int(round(h - (r + minRad) * np.sin((th * np.pi) / 180))) 
                     if a >= 0 and a < img.shape[1] and b >= 0 and b < img.shape[0]:
                         acc[a, b, r] += 1
                         #print(acc[a, b, r])
@@ -57,26 +74,26 @@ notPart = 0
 #print(np.max(acc[a-1:a+1, b-1:b+1, r-1:r+1]))
 end = time.perf_counter()
 print(end - start)
-acc=np.pad(acc, ((1,1),(1,1),(1,1)), mode="constant", constant_values=0)
-for r  in range(acc.shape[2] - 1,  1, -1):
-    for a in range(1, acc.shape[0] - 1):
-        for b in range(1, acc.shape[1] - 1):
-            if r + minRad - 1 > a:
+#acc=np.pad(acc, ((1,1),(1,1),(1,1)), mode="constant", constant_values=0)
+for r  in range(acc.shape[2] - 1,  0, -1):
+    for a in range(0, acc.shape[0]):
+        for b in range(0, acc.shape[1]):
+            if r + minRad > a:
                 radSumA = a
-            elif r + minRad - 1 > acc.shape[0] - a:
+            elif r + minRad > acc.shape[0] - a:
                 radSumA = acc.shape[0] - a
             else:
-                radSumA =  r + minRad -1
-            if r + minRad - 1 > b:
+                radSumA =  r + minRad
+            if r + minRad > b:
                 radSumB = b
-            elif r + minRad - 1 > acc.shape[1] - b:
+            elif r + minRad > acc.shape[1] - b:
                 radSumA = acc.shape[0] - b
             else:
-                radSumB =  r + minRad - 1
-            if acc[a, b, r] > 150 and np.max(acc[a-radSumA:a+radSumA, b-radSumB:b+radSumB, r-1:r+1]) <= acc[a, b, r]:
+                radSumB =  r + minRad
+            if acc[a, b, r] > 100 and np.max(acc[a-radSumA:a+radSumA, b-radSumB:b+radSumB, r-1:r+1]) <= acc[a, b, r]:
                 for th in range(0, 360):
-                    x = int(round(a - (r + minRad - 1) * np.cos((th * np.pi) / 180)))
-                    y = int(round(b - (r + minRad - 1) * np.sin((th * np.pi) / 180)))
+                    x = int(round(a - (r + minRad) * np.cos((th * np.pi) / 180))) 
+                    y = int(round(b - (r + minRad) * np.sin((th * np.pi) / 180))) 
                     if x >= 0 and x < img.shape[1] and y >= 0 and y < img.shape[0]:
                         if (x, y) not in circlePoints:
                             circlePoints.append((x, y))
@@ -84,15 +101,32 @@ for r  in range(acc.shape[2] - 1,  1, -1):
                                 isPart = isPart + 1
                             else:
                                 notPart = notPart + 1
-                if isPart / (isPart + notPart) >= 0.0:
-                    cv2.circle(imgOrg, (a - 1, b - 1), r + minRad - 1, (0, 0, 255), 1)
-                    acc[a-radSumA:a+radSumA, b-radSumB:b+radSumB, 0:r] = 0
+                if isPart / (isPart + notPart) >= 0.4:
+                    if r + minRad + minimumDistanceBetweenCircles > a:
+                        radSumAneg = a
+                    else:
+                        radSumAneg =  r + minRad + minimumDistanceBetweenCircles
+                    if r + minRad + minimumDistanceBetweenCircles > acc.shape[0] - a:
+                        radSumApos = acc.shape[0] - a
+                    else:
+                        radSumApos =  r + minRad + minimumDistanceBetweenCircles
+                    if r + minRad + minimumDistanceBetweenCircles > b:
+                        radSumBneg = b
+                    else:
+                        radSumBneg =  r + minRad + minimumDistanceBetweenCircles
+                    if r + minRad + minimumDistanceBetweenCircles > acc.shape[1] - b:
+                        radSumBpos = acc.shape[1] - b
+                    else:
+                        radSumBpos =  r + minRad + minimumDistanceBetweenCircles
+                    cv2.circle(imgOrg, (a, b), r + minRad, (0, 0, 255), 1)
+                    acc[a-radSumAneg:a+radSumApos, b-radSumBneg:b+radSumBpos, 0:maxRad-minRad] = 0
                 circlePoints.clear()
                 isPart = 0
                 notPart = 0
 end = time.perf_counter()
 print(end - start)
 cv2.imshow("img", imgOrg)
+cv2.imshow("And", img_and)
 cv2.imwrite("houghCircle.png", imgOrg)
 cv2.waitKey()
 cv2.destroyAllWindows()
--- houghCircleTransformCV.py
+++ houghCircleTransformCV.py
@@ -10,3 +10,19 @@ img = cv2.cvtColor(imgOrg, cv2.COLOR_BGR2GRAY)
 sobelx = cv2.Sobel(img, cv2.CV_32F , 1, 0, ksize=3)
 sobely = cv2.Sobel(img, cv2.CV_32F , 0, 1, ksize=3)
 mag, ang = cv2.cartToPolar(sobelx, sobely)
+print(ang.shape)
+acc = np.zeros((img.shape[1], img.shape[0]))
+minRad = 2
+maxRad = 10
+for w in range(0, img.shape[1] - 1):
+    for h in range(0, img.shape[0] - 1):
+        if img[h,w] == 0:
+            for r in range(minRad,maxRad):
+                X = int(round(w + (r * np.cos(ang[h,w]))))
+                Y = int(round(h + (r * np.sin(ang[h,w]))))
+                if X >= 0 and X < img.shape[1] and Y >= 0 and Y < img.shape[0]:
+                    acc[X,Y] = acc[X,Y] + 1
+            
+cv2.imshow("acc",acc)
+cv2.waitKey()
+print(np.where(acc == np.amax(acc)))
